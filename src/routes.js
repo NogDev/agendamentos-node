dc1b16f8a08e1b0d15871162e36123a011f50a77
nog <nogueira@pm.ba.gov.br>
2020-05-13 21:44:39 -0300

    modificando sintase com o sucrase

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
@@ -8,4 +8,4 @@ routes.get('/', (req, res) => {
   });
 })
 
-module.exports = routes;
+export default routes;
