249f13a2ec1e50ce6c8f321e4d9931c938fc2959
nog <nogueira@pm.ba.gov.br>
2020-05-15 00:00:30 -0300

    implementando update de usuários

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -18,7 +18,29 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.json({ ok: true });
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExist = await User.findOne({ where: { email } });
+
+      if (userExist) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not macht' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
