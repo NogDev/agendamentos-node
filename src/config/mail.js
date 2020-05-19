dba5bd7c49a188bd2d1a7cea504754e2290a6de1
nog <nogueira@pm.ba.gov.br>
2020-05-19 15:02:05 -0300

    enviando email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '9e5389ad6265f4',
+    pass: '005949761aaf18',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
